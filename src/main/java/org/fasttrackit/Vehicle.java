a91b5cda25dc02179bce4b4413e56274a576adcd
VescanRemus <remus@fasttrackit.org>
2021-04-20 14:14:09 +0300

    stutied has a relationship

--- src/main/java/org/fasttrackit/Vehicle.java
+++ src/main/java/org/fasttrackit/Vehicle.java
@@ -10,4 +10,23 @@ public class Vehicle {
     boolean damaged;
     String color;
 
+    public double accelerate (double speed, double durationInHours) {
+
+        System.out.println(name+" is accelerating with "+ speed + " km/h for " + durationInHours +"h");
+
+        double distance = speed * durationInHours;
+
+        totalTravelDistance += distance;
+
+        System.out.println("Total travel distance for vehicle " +name + " :" + totalTravelDistance);
+
+        double usedFuel =distance * mileage / 100;
+
+        fuelLevel -= usedFuel;
+
+        System.out.println("Remaining fuel for vehicle " +name + " :" + fuelLevel);
+
+        return distance;
+
+    }
 }
